6ef1b1f3f8adf95fc7b0a2b63ac8551973bf51ea
AlexLeCrow <akac92@uw.edu>
2015-04-16 22:15:02 -0700

    Main Activity is coming along, maple is default screen, implemented pip color border, and have a custom marker color to match. Sliding from the left brings out the drawerlayout (currently empty).

--- app/src/main/java/com/tcss450/moneyteam/geotracker/MainActivity.java
+++ app/src/main/java/com/tcss450/moneyteam/geotracker/MainActivity.java
@@ -11,9 +11,11 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.animation.AnimationUtils;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
@@ -23,7 +25,9 @@ import android.widget.Toast;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.util.ArrayList;
@@ -34,6 +38,7 @@ import de.greenrobot.event.EventBus;
  * Default map activity
  */
 public class MainActivity extends FragmentActivity {
+    private static final String MAIN_ACTIVITY_ERROR_KEY = "MAIN";
     private String[] mItemTitles;
     private DrawerLayout mDrawerLayout;
     private ListView mDrawerList;
@@ -50,159 +55,20 @@ public class MainActivity extends FragmentActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_maps);
-        setUpMapIfNeeded();
+        setContentView(R.layout.activity_main);
 
         mTitle = mDrawerTitle = getTitle();
         mItemTitles = getResources().getStringArray(R.array.drawer_titles_array);
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        mDrawerList = (ListView) findViewById(R.id.left_drawer);
-        //TODO Add references to both Home and Account Settings Fragments
+        mDrawerList = (ListView) findViewById(R.id.drawer);
 
-        // Set the adapter for the list view
-        mDrawerList.setAdapter(new ArrayAdapter<String>(this,
-                R.layout.drawer_list_item, mItemTitles));
-        // Set the list's click listener
-        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
-
-        //TODO chance the icon to something customized
-        mDrawerToggle = new ActionBarDrawerToggle(this,mDrawerLayout, R.string.open_drawer,R.string.close_drawer) {
-            /** Called when a drawer has settled in a completely closed state. */
-            public void onDrawerClosed(View view) {
-                super.onDrawerClosed(view);
-                getActionBar().setTitle(mTitle);
-                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
-            }
-
-            /** Called when a drawer has settled in a completely open state. */
-            public void onDrawerOpened(View drawerView) {
-                super.onDrawerOpened(drawerView);
-                getActionBar().setTitle(mDrawerTitle);
-                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
-            }
-        };
-        // Set the drawer toggle as the DrawerListener
-        mDrawerLayout.setDrawerListener(mDrawerToggle);
-
-        getActionBar().setDisplayHomeAsUpEnabled(true);
-        getActionBar().setHomeButtonEnabled(true);
-    }
-
-    @Override
-    protected void onPostCreate(Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
-        // Sync the toggle state after onRestoreInstanceState has occurred.
-        mDrawerToggle.syncState();
-    }
-
-    @Override
-    public void onConfigurationChanged(Configuration newConfig) {
-        super.onConfigurationChanged(newConfig);
-        mDrawerToggle.onConfigurationChanged(newConfig);
     }
 
 
-    /* Called whenever we call invalidateOptionsMenu() */
-    @Override
-    public boolean onPrepareOptionsMenu(Menu menu) {
-        // If the nav drawer is open, hide action items related to the content view
-        boolean drawerOpen = mDrawerLayout.isDrawerOpen(mDrawerList);
-        //CAN Adjust menu item visibility to reduce redundancy
-        //menu.findItem(R.id.action_websearch).setVisible(!drawerOpen);
-        return super.onPrepareOptionsMenu(menu);
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        //Handle Sign Out functionality
-        if (id == R.id.action_sign_out) {
-            Intent loginScreen = new Intent(getApplicationContext(), LoginActivity.class);
-            //key,values to send to main.
-            Log.e("d", "Succesfully logged out.");
-            startActivity(loginScreen);
-            overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
-            return true;
-        }
-
-        // Pass the event to ActionBarDrawerToggle, if it returns
-        // true, then it has handled the app icon touch event
-        //For redundant implementations
-        if (mDrawerToggle.onOptionsItemSelected(item)) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    /** Swaps fragments in the main content view
-     * Position Table:
-     * 0] Home
-     * 1] Map (default)
-     * 2] Account Settings
-     * */
-    private void selectItem(int position) {
-        String titleString = getString(R.string.main_map_title);
-
-        if(position != 1) {
-            // Insert the fragment
-            FragmentManager fragmentManager = getFragmentManager();
-            if(position == 0) {
-                fragmentManager.beginTransaction()
-                        .replace(R.id.content_frame, mHomeFragment)
-                        .commit();
-                titleString = getString(R.string.main_home_title);
-            }
-            else if(position == 1) {
-                fragmentManager.beginTransaction()
-                        .replace(R.id.content_frame, mAccountSettingsFragment)
-                        .commit();
-                titleString = getString(R.string.main_account_settings_title);
-            }
-        }
-
-        // Highlight the selected item, update the title, and close the drawer
-        mDrawerList.setItemChecked(position, true);
-        setTitle(titleString);
-        mDrawerLayout.closeDrawer(mDrawerList);
-    }
-
-    @Override
-    public void setTitle(CharSequence title) {
-        mTitle = title;
-        getActionBar().setTitle(mTitle);
-    }
-
-    /**
-     * Created by Alex on 4/16/2015.
-     */
-    public class DrawerItemClickListener implements ListView.OnItemClickListener {
-        @Override
-        public void onItemClick(AdapterView parent, View view, int position, long id) {
-            selectItem(position);
-        }
-    }
-
-    // GOOGLE MAPS FUNCTIONALITY ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    // GOOGLE MAPS FUNCTIONALITY (DO NOT TOUCH)~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     @Override
     protected void onResume() {
+        Log.e(MAIN_ACTIVITY_ERROR_KEY, "onResume");
         super.onResume();
         setUpMapIfNeeded();
     }
@@ -223,6 +89,7 @@ public class MainActivity extends FragmentActivity {
      * method in {@link #onResume()} to guarantee that it will be called.
      */
     private void setUpMapIfNeeded() {
+        Log.e(MAIN_ACTIVITY_ERROR_KEY, "setUpMapIfNeeded");
         // Do a null check to confirm that we have not already instantiated the map.
         if (mMap == null) {
             // Try to obtain the map from the SupportMapFragment.
@@ -242,7 +109,11 @@ public class MainActivity extends FragmentActivity {
      * This should only be called once and when we are sure that {@link #mMap} is not null.
      */
     private void setUpMap() {
-        mMap.addMarker(new MarkerOptions().position(new LatLng(SEATTLE_LAT, SEATTLE_LONG)).title("Marker"));
+        Log.e(MAIN_ACTIVITY_ERROR_KEY, "setUpMap");
+
+        Marker defaultMarker = mMap.addMarker(new MarkerOptions().position(new LatLng(0,0)).title("Fallout Seattle Zone")
+                .icon(BitmapDescriptorFactory
+                        .defaultMarker(140.0F)));
     }
 }
 
--- app/src/main/java/com/tcss450/moneyteam/geotracker/PipMapFragment.java
+++
@@ -1,9 +0,0 @@
-package com.tcss450.moneyteam.geotracker;
-
-import android.app.Fragment;
-
-/**
- * Created by Alex on 4/16/2015.
- */
-public class PipMapFragment extends Fragment {
-}
