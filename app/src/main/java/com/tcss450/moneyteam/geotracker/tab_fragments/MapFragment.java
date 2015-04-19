86ec44d5cc9180e0b3f5f8e95398c5143b837b5f
AlexLeCrow <akac92@uw.edu>
2015-04-18 22:13:17 -0700

    Lots of changes check out account and map settings

--- app/src/main/java/com/tcss450/moneyteam/geotracker/tab_fragments/MapFragment.java
+++ app/src/main/java/com/tcss450/moneyteam/geotracker/tab_fragments/MapFragment.java
@@ -10,6 +10,7 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.BitmapDescriptor;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CameraPosition;
@@ -24,7 +25,7 @@ import com.tcss450.moneyteam.geotracker.R;
 public class MapFragment extends Fragment {
 
     MapView mMapView;
-    private GoogleMap googleMap;
+    private GoogleMap mMap;
     private LatLng mSeattleLatLng;
 
     @Override
@@ -38,33 +39,34 @@ public class MapFragment extends Fragment {
 
         mMapView.onResume();// needed to get the map to display immediately
 
-        //SETUP LATLNG MARKERS
-        final double seattleLat = 47.6097;
-        final double seattleLng = 122.3331;
-        mSeattleLatLng = new LatLng(seattleLat, seattleLng);
-
         try {
             MapsInitializer.initialize(getActivity().getApplicationContext());
         } catch (Exception e) {
             e.printStackTrace();
         }
 
-        googleMap = mMapView.getMap();
+        mMap = mMapView.getMap();
 
         //GENERATE MARKER~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-        MarkerOptions marker = new MarkerOptions().position(
-                new LatLng(seattleLat, seattleLng)).title("Seattle Fallout Zone");
-        marker.icon(BitmapDescriptorFactory
-                .defaultMarker(140f));
+                LatLng sydney = new LatLng(-33.867, 151.206);
 
-        // adding marker
+        //SETUP MAP~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        mMap.setMyLocationEnabled(true);
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 13));
+
+        mMap.addMarker(new MarkerOptions()
+                .title("Sydney")
+                .snippet("The most populous city in Australia.")
+                .position(sydney));
+
+        /* adding marker
         googleMap.addMarker(marker);
         CameraPosition cameraPosition = new CameraPosition.Builder()
                 .target(mSeattleLatLng).zoom(8).build();
         googleMap.animateCamera(CameraUpdateFactory
                 .newCameraPosition(cameraPosition));
 
-        // Perform any camera updates here
+        // Perform any camera updates here*/
         return v;
     }
 
