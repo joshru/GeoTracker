43583e9083be1b8acc7c75ac2d1c8842b6f3899a
AlexLeCrow <akac92@uw.edu>
2015-04-18 21:45:05 -0700

    Lots of changes check out account and map settings

--- app/src/main/java/com/tcss450/moneyteam/geotracker/tab_fragments/MapFragment.java
+++ app/src/main/java/com/tcss450/moneyteam/geotracker/tab_fragments/MapFragment.java
@@ -6,16 +6,89 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.GroundOverlayOptions;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.tcss450.moneyteam.geotracker.R;
 
 /**
  * Created by Alex on 4/18/2015.
  */
 public class MapFragment extends Fragment {
+
+    MapView mMapView;
+    private GoogleMap googleMap;
+    private LatLng mSeattleLatLng;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_map, container, false);
-        return rootView;
+        // inflat and return the layout
+        View v = inflater.inflate(R.layout.fragment_map, container,
+                false);
+        mMapView = (MapView) v.findViewById(R.id.mapView);
+        mMapView.onCreate(savedInstanceState);
+
+        mMapView.onResume();// needed to get the map to display immediately
+
+        //SETUP LATLNG MARKERS
+        final double seattleLat = 47.6097;
+        final double seattleLng = 122.3331;
+        mSeattleLatLng = new LatLng(seattleLat, seattleLng);
+
+        try {
+            MapsInitializer.initialize(getActivity().getApplicationContext());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        googleMap = mMapView.getMap();
+
+        //GENERATE MARKER~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        MarkerOptions marker = new MarkerOptions().position(
+                new LatLng(seattleLat, seattleLng)).title("Seattle Fallout Zone");
+        marker.icon(BitmapDescriptorFactory
+                .defaultMarker(140f));
+
+        // adding marker
+        googleMap.addMarker(marker);
+        CameraPosition cameraPosition = new CameraPosition.Builder()
+                .target(mSeattleLatLng).zoom(8).build();
+        googleMap.animateCamera(CameraUpdateFactory
+                .newCameraPosition(cameraPosition));
+
+        // Perform any camera updates here
+        return v;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mMapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mMapView.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mMapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mMapView.onLowMemory();
     }
 }
