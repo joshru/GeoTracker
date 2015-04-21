b81af53e94ccb351ec63cdc6a12681203de1558f
AlexLeCrow <akac92@uw.edu>
2015-04-20 20:35:55 -0700

    Comments/todos for josh

--- app/src/main/java/com/tcss450/moneyteam/geotracker/fragments/TrackingFragment.java
+++ app/src/main/java/com/tcss450/moneyteam/geotracker/fragments/TrackingFragment.java
@@ -9,12 +9,15 @@ import android.view.ViewGroup;
 import com.tcss450.moneyteam.geotracker.R;
 
 /**
- * Created by Alex on 4/18/2015.
+ * TODO
+ * Activity for handling logic inside the tracking settings fragment.
+ * @author Alexander Cherry(akac92@uw.edu)
  */
 public class TrackingFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        //GET REFERENCE TO ROOTVIEW AND INFLATE FRAGMENT~~~~~~~~~~~~~~~~~~~~~~~~~~~~
         View rootView = inflater.inflate(R.layout.fragment_tracking_settings, container, false);
         return rootView;
     }
