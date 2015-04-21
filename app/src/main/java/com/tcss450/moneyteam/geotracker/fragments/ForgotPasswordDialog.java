f7712d020ab726980854465443e5d5f86c7358b0
AlexLeCrow <akac92@uw.edu>
2015-04-20 20:27:05 -0700

    Comments/todos for josh

--- app/src/main/java/com/tcss450/moneyteam/geotracker/fragments/ForgotPasswordDialog.java
+++ app/src/main/java/com/tcss450/moneyteam/geotracker/fragments/ForgotPasswordDialog.java
@@ -17,12 +17,12 @@ import com.tcss450.moneyteam.geotracker.Authenticator;
 import com.tcss450.moneyteam.geotracker.R;
 
 /**
- * Created by Brandon on 4/16/2015.
+ * TODO JAVADOC
+ * @author  Brandon Bell
  */
 public class ForgotPasswordDialog extends DialogFragment {
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
+    /** TODO */
     private EditText mNewPassText;
     private EditText mNewPassRepeatText;
     private TextView mDialogUserQuestion;
@@ -30,52 +30,61 @@ public class ForgotPasswordDialog extends DialogFragment {
     private Button mDialogSubmitButton;
     private boolean mResetSuccessful;
 
+    /**
+     * TODO
+     * @return
+     */
     public static ForgotPasswordDialog newInstance() {
         ForgotPasswordDialog fragment = new ForgotPasswordDialog();
-        //Bundle args = new Bundle();
-        ////args.putString(ARG_PARAM1, param1);
-        ////args.putString(ARG_PARAM2, param2);
-       // fragment.setArguments(args);
         return fragment;
     }
 
+    /**
+     * TODO
+     * @param savedInstanceState
+     * @return
+     */
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
+        //FIELD INSTANTIATION~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
         mResetSuccessful = false;
+        final String question;
+        final String correctAnswer;
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        final AlertDialog dialog;
+
+        //INFLATE THIS FRAGMENT~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
         View v = getActivity().getLayoutInflater().inflate(R.layout.fragment_forgot_password_dialog, null);
 
+        //REFERENCES TO VIEWS~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
         mNewPassText = (EditText) v.findViewById((R.id.dialog_security_newpass));
         mNewPassRepeatText = (EditText) v.findViewById(R.id.dialog_security_repeatpass);
         mDialogUserQuestion = (TextView) v.findViewById(R.id.dialog_security_user_question);
         mDialogSecurityAnswer = (EditText) v.findViewById(R.id.dialog_security_answer);
-        mDialogSubmitButton = (Button) v.findViewById(R.id.dialog_submit_button);//return super.onCreateDialog(savedInstanceState);
+        mDialogSubmitButton = (Button) v.findViewById(R.id.dialog_submit_button);
 
-        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-      //  LayoutInflater inflater = mActivity.getLayoutInflater();
-
-        //Obtain security question and answer
+        //GET SHARED PREFERENCES~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
         SharedPreferences myPreferences = getActivity()
                 .getSharedPreferences(getString(R.string.shared_pref_key), Context.MODE_PRIVATE);
         final SharedPreferences.Editor myPrefEditor = myPreferences.edit();
-        final String question = myPreferences
+        //GET RESOURCES FROM SHARED PREFERENCES~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        question = myPreferences
                 .getString(getString(R.string.saved_question_key),
                         getString(R.string.default_restore_key));
-        final String correctAnswer = myPreferences
+        correctAnswer = myPreferences
                 .getString(getString(R.string.saved_question_answer_key),
                         getString(R.string.default_restore_key));
 
-
-
-        /* USE the .setVisibility(View.VISIBLE) to make the password text visible once the user
-        enters the correct answer.
-
-*/
+        //DIALOG SETTINGS/ONCLICK LISTENER~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
         mDialogUserQuestion.setText(question);
-
         mDialogSubmitButton.setOnClickListener(new View.OnClickListener() {
+            /**
+             * TODO
+             * @param v
+             */
             @Override
             public void onClick(View v) {
-
+                //SECURITY QUESTION ANSWERED CORRECTLY~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
                 if (mDialogSecurityAnswer.getText().toString().equals(correctAnswer)) {
                     mNewPassText.setVisibility(View.VISIBLE);
                     mNewPassRepeatText.setVisibility(View.VISIBLE);
@@ -86,21 +95,24 @@ public class ForgotPasswordDialog extends DialogFragment {
                     Toast.makeText(getActivity(), getString(R.string.toast_security_incorrect),
                             Toast.LENGTH_LONG).show();
                 }
-
             }
         });
 
-      //  mFragmentView = inflater.inflate(R.layout.fragment_forgot_password_dialog, null);
-
+        //SETS THE VIEW OF THE ALERT DIALOG BOX~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
         builder.setView(v)
-                //.setCancelable(false)
+                //SETS ACTION FOR POSITIVE BUTTON~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
                 .setPositiveButton(R.string.reset_confirm_button, new DialogInterface.OnClickListener() {
-
+                    /**
+                     * TODO
+                     * @param dialog
+                     * @param which
+                     */
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-                     //blank, manually overriden later
+                     //Intentionally left blank, manually overriden later
                     }
                 })
+                //SETS ACTION FOR NEGATIVE BUTTON~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
                 .setNegativeButton(R.string.reset_cancel_button, new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
@@ -111,20 +123,25 @@ public class ForgotPasswordDialog extends DialogFragment {
                         //dialog.cancel();
                     }
                 });
-
-        final AlertDialog dialog = builder.create();
+        //BUILD THE ALERT DIALOG~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        dialog = builder.create();
         dialog.show();
 
+        //GET THE PROPER BUTTON~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
         dialog.getButton(AlertDialog.BUTTON_POSITIVE).setOnClickListener(new View.OnClickListener() {
+            /**
+             * TODO
+             * @param v
+             */
             @Override
             public void onClick(View v) {
+                //CHECKS THAT NEW PASSWORD IS FORMATTED PROPERLY AND matches REPEAT~
                 if (Authenticator.passFormatCheck(mNewPassText.getText().toString())
                         && mNewPassRepeatText
                         .getText().toString().equals(mNewPassText.getText().toString())) {
                     myPrefEditor.putString(getString(R.string.saved_pass_key),
                             Authenticator.generateHash(mNewPassText.getText().toString()));
                     myPrefEditor.apply();
-
 
                     mResetSuccessful = true;
 
@@ -137,7 +154,6 @@ public class ForgotPasswordDialog extends DialogFragment {
                     Toast.makeText(getActivity(), getString(R.string.toast_security_password_changed),
                             Toast.LENGTH_LONG).show();
                 }
-
             }
         });
 
