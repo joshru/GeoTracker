376228719a1c440635b28487a8ea26a14c62022c
AlexLeCrow <akac92@uw.edu>
2015-05-28 13:26:48 -0700

    Intent listeners work now.

--- app/src/main/java/com/tcss450/moneyteam/geotracker/Utilities/BootLoader.java
+++ app/src/main/java/com/tcss450/moneyteam/geotracker/Utilities/BootLoader.java
@@ -20,34 +20,35 @@ public class BootLoader extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        if(context != null) {
+        Log.i("ANDROID INTENT", "BootLoader intent recieved, " + intent + " Context: " + context);
 
-            SharedPreferences sp = context.getSharedPreferences(context.getString(R.string.user_info_main_key), Context.MODE_PRIVATE);
-            Boolean locationTrackBool = sp.getBoolean(context.getString(R.string.saved_location_toggle_boolean), false);
-            int locationTimer = sp.getInt(context.getString(R.string.key_location_poll_timer), 0);
-            int spinnerPos = sp.getInt(context.getString(R.string.saved_spinner_position), 0);
-            boolean onConnected = sp.getBoolean(context.getString(R.string.saved_on_connected_bool), false);
+        String intentAction = intent.getAction();
+        SharedPreferences sp = context.getSharedPreferences(context.getString(R.string.user_info_main_key), Context.MODE_PRIVATE);
+        Boolean locationTrackBool = sp.getBoolean(context.getString(R.string.saved_location_toggle_boolean), false);
+        int locationTimer = sp.getInt(context.getString(R.string.key_location_poll_timer), 0);
+        int spinnerPos = sp.getInt(context.getString(R.string.saved_spinner_position), 0);
+        boolean onConnected = sp.getBoolean(context.getString(R.string.saved_on_connected_bool), false);
 
-            //Handles BOOT_COMPLETED intent
-            if (intent.getAction().equals("android.intent.action.BOOT_COMPLETED")) {
-                locationTimer = (locationTimer <= 0) ? (1):(locationTimer);
-                LocationIntentService.setServiceAlarm(context, locationTrackBool, locationTimer);
-                WebPushIntent.setServerAlarm(context, locationTrackBool, spinnerPos);
-            }
+        //Handles BOOT_COMPLETED intent
+        if (Intent.ACTION_BOOT_COMPLETED.equalsIgnoreCase(intentAction)) {
+            locationTimer = (locationTimer <= 0) ? (1) : (locationTimer);
+            LocationIntentService.setServiceAlarm(context, locationTrackBool, locationTimer);
+            WebPushIntent.setServerAlarm(context, locationTrackBool, spinnerPos);
+        }
 
-            //Handles BATTERY_LOW intent
-            if (intent.getAction().equals("android.intent.action.BATTERY_LOW")) {
+        //Handles BATTERY_LOW intent
+        if (Intent.ACTION_BATTERY_LOW.equalsIgnoreCase(intentAction)) {
                 /* Force stop all network services. */
-                LocationIntentService.setServiceAlarm(context, false, locationTimer);
-                WebPushIntent.setServerAlarm(context, false, 0);
-            }
+            LocationIntentService.setServiceAlarm(context, false, locationTimer);
+            WebPushIntent.setServerAlarm(context, false, 0);
+        }
 
-            //Handles ACTION_POWER_CONNECTED intent
-            if (intent.getAction().equals("android.intent.action.ACTION_POWER_CONNECTED") && onConnected) {
-                Log.i("POWER CONNECTED", "Points have been pushed to the server");
-                LocationDBHelper db = new LocationDBHelper(context);
-                db.pushPointsToServer();
-            }
+        //Handles ACTION_POWER_CONNECTED intent
+        if (Intent.ACTION_POWER_CONNECTED.equalsIgnoreCase(intentAction) && onConnected) {
+            Log.i("POWER CONNECTED", "Points have been pushed to the server");
+            Log.i("POWER CONNECTED", "onConnected Boolean: " + onConnected);
+            LocationDBHelper db = new LocationDBHelper(context);
+            db.pushPointsToServer();
         }
     }
 }
